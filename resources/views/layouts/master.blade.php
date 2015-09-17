5c335577b253af5c1b048c2314308e8fa96004b4
Hayato Kihara <hayato@MacBookPro.local>
2015-09-18 05:56:20 +0900

    ヘッダとフッタ

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
       <style type="text/css">
         .main-picture img {
-          width: 75%;
+          width: 100%;
         }
         body {
           background-color: #fffacd;
@@ -36,6 +36,15 @@
           height: 30px;
           color: white;
           background-color: #DC143C;
+          text-align: center;
+        }
+        .pengmin {
+          text-align: left;
+          vertical-align: top;
+        }
+        span.rakuten {
+          left: 500px;
+
         }
 
       </style>
@@ -48,13 +57,18 @@
     </head>
     <body>
       <div class="header">
-        <h3>PenGumin           楽天クエスト</h3>
-        <form method="GET" action="{{action('MypageController@index')}}" accept-charset="UTF-8" class="mypage">
-          <button type="submit" class="btn btn-success">MyPage</button>
-        </form>
-        <form method="GET" action="{{action('Auth\AuthController@getLogout')}}" accept-charset="UTF-8" class="logout">
-          <button type="submit" class="btn btn-info">Logout</button>
-        </form>
+        <div class="pengumin col-md-1"><h4>PenGumin</h4></div>
+        <div class="rakuten col-md-3"><h4>楽天クエスト</h4></div>
+        <div class="mypage col-md-offset-6 col-md-1">
+          <form method="GET" action="{{action('MypageController@index')}}" accept-charset="UTF-8">
+            <button type="submit" class="btn btn-success">MyPage</button>
+          </form>
+        </div>
+        <div class="logout col-md-1">
+          <form method="GET" action="{{action('Auth\AuthController@getLogout')}}" accept-charset="UTF-8" class="logout">
+            <button type="submit" class="btn btn-info">Logout</button>
+          </form>
+        </div>
       </div>
       <div class="main-picture">
         <img src="http://www.fastpic.jp/images.php?file=9838842752.gif" class="img-responsive center-block" alt="logo">
@@ -63,7 +77,9 @@
         @yield('body')
       </div>
       <div class="footer">
-
+        <div class="mess">
+          <h5>Copyright © 2015 PenGumin All Rights Reserved.</h5>
+        </div>
       </div>
     </body>
 
